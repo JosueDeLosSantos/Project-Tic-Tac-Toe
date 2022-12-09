27bbcd9d73a7c646acfd2fcec3d0f0d1df8a2e51
Josue <jdd1993@hotmail.com>
2022-12-08 21:46:46 -0400

    Add logic that keeps players from playing in spots that are already taken.

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ const clicker = document.querySelectorAll('.box')
 const data = document.querySelectorAll('[data-box-number]')
 
 
+//Players factory function
+const playerFactory = (selection, index) => {
+  const announcement = () => console.log(selection + ' is the winner!')
+  return{selection, index, announcement}
+}
+
+const player1 = playerFactory('x')
+const player2 = playerFactory('o')
 
 //Gameboard module
 const Gameboard = (() => {
@@ -28,58 +36,51 @@ const displayController = (() => {
 
    const controller = (board) => {
     
-    if (board[0] =='x' && board[1] =='x' && board[2] =='x') console.log('x is the winner')
-    if (board[3] =='x' && board[4] =='x' && board[5] =='x') console.log('x is the winner')
-    if (board[6] =='x' && board[7] =='x' && board[8] =='x') console.log('x is the winner')
-    if (board[0] =='x' && board[3] =='x' && board[6] =='x') console.log('x is the winner')
-    if (board[1] =='x' && board[4] =='x' && board[7] =='x') console.log('x is the winner')
-    if (board[2] =='x' && board[5] =='x' && board[8] =='x') console.log('x is the winner')
-    if (board[0] =='x' && board[4] =='x' && board[8] =='x') console.log('x is the winner')
-    if (board[2] =='x' && board[4] =='x' && board[6] =='x') console.log('x is the winner')
+    if (board[0] =='x' && board[1] =='x' && board[2] =='x') player1.announcement()
+    if (board[3] =='x' && board[4] =='x' && board[5] =='x') player1.announcement()
+    if (board[6] =='x' && board[7] =='x' && board[8] =='x') player1.announcement()
+    if (board[0] =='x' && board[3] =='x' && board[6] =='x') player1.announcement()
+    if (board[1] =='x' && board[4] =='x' && board[7] =='x') player1.announcement()
+    if (board[2] =='x' && board[5] =='x' && board[8] =='x') player1.announcement()
+    if (board[0] =='x' && board[4] =='x' && board[8] =='x') player1.announcement()
+    if (board[2] =='x' && board[4] =='x' && board[6] =='x') player1.announcement()
 
-    if (board[0] =='o' && board[1] =='o' && board[2] =='o') console.log('o is the winner')
-    if (board[3] =='o' && board[4] =='o' && board[5] =='o') console.log('o is the winner')
-    if (board[6] =='o' && board[7] =='o' && board[8] =='o') console.log('o is the winner')
-    if (board[0] =='o' && board[3] =='o' && board[6] =='o') console.log('o is the winner')
-    if (board[1] =='o' && board[4] =='o' && board[7] =='o') console.log('o is the winner')
-    if (board[2] =='o' && board[5] =='o' && board[8] =='o') console.log('o is the winner')
-    if (board[0] =='o' && board[4] =='o' && board[8] =='o') console.log('o is the winner')
-    if (board[2] =='o' && board[4] =='o' && board[6] =='o') console.log('o is the winner')
+    if (board[0] =='o' && board[1] =='o' && board[2] =='o') player2.announcement()
+    if (board[3] =='o' && board[4] =='o' && board[5] =='o') player2.announcement()
+    if (board[6] =='o' && board[7] =='o' && board[8] =='o') player2.announcement()
+    if (board[0] =='o' && board[3] =='o' && board[6] =='o') player2.announcement()
+    if (board[1] =='o' && board[4] =='o' && board[7] =='o') player2.announcement()
+    if (board[2] =='o' && board[5] =='o' && board[8] =='o') player2.announcement()
+    if (board[0] =='o' && board[4] =='o' && board[8] =='o') player2.announcement()
+    if (board[2] =='o' && board[4] =='o' && board[6] =='o') player2.announcement()
   }
 
   return {controller}
 
 })()
 
-//Players factory function
-const playerFactory = (selection, index) => {
-  const announcement = () => console.log(selection + ' is the winner!')
-  return{selection, index, announcement}
-}
-
 let toggle = 0
 
 function show(e) {
+
   let boxId = e.target.id
-  let player1 = playerFactory('x', boxId)
-  let player2 = playerFactory('o', boxId)
+  if (e.target.hasAttribute('data-marker')) return
   toggle++
 
   if(toggle == 1) {
     Gameboard.player(player1.selection, boxId)
     displayController.controller(Gameboard.player(player1.selection, boxId))
-    e.target.dataset.boxNumber = player1.selection
+    e.target.dataset.marker = player1.selection
     e.target.innerText = player1.selection
   }
+
   if (toggle == 2) {
     Gameboard.player(player2.selection, boxId)
     displayController.controller(Gameboard.player(player2.selection, boxId))
-    e.target.dataset.boxNumber = player2.selection
+    e.target.dataset.marker = player2.selection
     e.target.innerText = player2.selection
     toggle = 0
   }
-  
-  console.log(window.document.body.children[0])
   
 }
 
